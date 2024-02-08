140f463ff1606c7ac8fcc499a75921be76ad89d9
이종현 <rmaskdkfhk@gmail.com>
2024-02-08 13:01:19 +0900

    rename reivew -> review

--- backend/src/main/java/com/lec/spring/repository/ReviewRepository.java
+++ backend/src/main/java/com/lec/spring/repository/ReviewRepository.java
@@ -3,5 +3,5 @@ package com.lec.spring.repository;
 import com.lec.spring.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReivewRepository extends JpaRepository<Review, Integer> {
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
 }
