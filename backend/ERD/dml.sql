038004f5ea53b640b817d5439590e01843df2997
이종현 <rmaskdkfhk@gmail.com>
2024-02-08 13:29:43 +0900

    USER 도메인 ë auth 컬럼 수정

--- backend/ERD/dml.sql
+++ backend/ERD/dml.sql
@@ -1,1 +1,8 @@
 create database anbd;
+
+show tables;
+
+desc user;
+insert into user(auth, certification, star, phone_number, nickname, name, username, password, email, thumbnail)
+values ('ROLE_ADMIN',1,3.3,'01012341234','test_admin','이종현','testid','1234','test@email.com','default.jpeg');
+
