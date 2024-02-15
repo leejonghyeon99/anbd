3c05e15fd1edf2f3454e77d388ca7a79da04e4c7
rlagydms2 <rlagydms1@gmail.com>
2024-02-15 11:37:51 +0900

    02/15 login form 수정(to GOUN)

--- frontend/src/user/SignUp.js
+++ frontend/src/user/SignUp.js
@@ -1,78 +1,153 @@
-import React, { useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+    repassword: "",
+    name: "",
+    nickname: "",
+    phone_number: "",
+    email: "",
+    address: "",
+  });
 
-    const [user, setUser] = useState({
-        username : '',
-        password : '',
-        repassword : '',
-        name : '',
-        nickname : '',
-        phone_number : '',
-        email : '',
-        address : '',  
+  const [adr, setAdr] = useState([]);
+  const [usernameErr, setUsernameErr] = useState(false);
+  const [passwordErr, setPasswordErr] = useState(false);
+  const [rerswdErr, setRepswdErr] = useState(false);
+  const [nameErr, setNameErr] = useState(false);
+  const [nicknameErr, setNicknameErr] = useState(false);
+  const [phoneErr, setPhoneErr] = useState(false);
+  const [emailErr, setEmailErr] = useState(false);
+  const [adrError, setAdrError] = useState(false);
+
+  useEffect(() => {
+    setUser({
+      ...user,
+      address: adr.toString(),
     });
+  }, [adr]);
 
-    const formChange = (e) => {
-        const {name, value} = e.target;
-        setUser(prevUser => ({
-            ...prevUser,
-            [name]: value
-        }));
-    };
+  const formChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const validateUser = (e) => {
-        e.preventDefault();
+  const submitJoin = (e) => {
+    e.preventDefault();
 
-        const {repassword, ...signupdata} = user;
+    const { repassword, ...signupdata } = user;
 
-        if(user.password !== user.repassword){
-            alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
-            return;
+    if (user.password !== user.repassword) {
+      alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+      return;
+    }
+
+    fetch(`${process.env.REACT_APP_API_BASE_URL}/api/user/signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(signupdata),
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("회원가입");
+          return response.json();
+        } else {
+          throw new Error("회원가입 실패");
         }
+      })
+      .then((data) => {
+          alert("회원가입 성공");
+          navigate("/home");
+      })
+      .catch((Error) => {
+        alert(Error.message);
+      });
+  };
 
-        fetch("http://localhost:8080/api/user/signup", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(signupdata),
-        })
-        .then(response => {
-            if(response.ok){
-                return response.json();
-            } else {
-                throw new Error("회원가입 실패")
-            }
-        })
-        .then(data => {
-            alert("회원가입 성공");
-            navigate("/home")
-        })
-        .catch(Error => {
-            alert(Error.message);
-        });
-    };
+  const validateEmail = () => {};
 
-    const validateEmail = () => {};
-
-
-        return (
-            <div>
-                <Form>id : <input name = "username" value={user.username} onChange={formChange}/></Form>
-                <Form>password : <input type='password' name = "password" value={user.password} onChange={formChange}/></Form>
-                <Form>repassword : <input type='password' name = "repassword" value={user.repassword} onChange={formChange}/></Form>
-                <Form>이름 : <input name = "name" value={user.name} onChange={formChange}/></Form>
-                <Form>닉네임 : <input name = "nickname" value={user.nickname} onChange={formChange}/></Form>
-                <Form>핸드폰번호 : <input name = "phone_number" value={user.phone_number} onChange={formChange}/></Form>
-                <Form>email : <input name = "email" value={user.email} onChange={formChange}/><Button onClick={validateEmail}>이메일인증</Button></Form>
-                <Form>주소 :<input name = "address" value={user.address} onChange={formChange}/></Form>
-                <Button onClick={validateUser}>회원 가입</Button>
-            </div>
-        );
-    };    
+  return (
+    <div>
+      <div>
+        <h1>JOIN US</h1>
+      </div>
+      <Form onSubmit={submitJoin}>
+        <div>
+          <label htmlFor="username">
+            ID<small>* </small>
+          </label>
+          <input name="username" value={user.username} onChange={formChange} />
+        </div>
+        <div>
+          <label htmlFor="password">
+            비밀번호<small>(필수)</small>
+          </label>
+          <input
+            type="password"
+            name="password"
+            value={user.password}
+            onChange={formChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="repassword">
+            비밀번호 확인<small>* </small>
+          </label>
+          <input
+            type="password"
+            name="repassword"
+            value={user.repassword}
+            onChange={formChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="name">
+            이름<small>* </small>
+          </label>
+          <input name="name" value={user.name} onChange={formChange} />
+        </div>
+        <div>
+          <label htmlFor="nickname">
+            닉네임<small>* </small>
+          </label>
+          <input name="nickname" value={user.nickname} onChange={formChange} />
+        </div>
+        <div>
+        <label htmlFor="phone_number">
+            핸드폰번호<small>* </small> 
+          </label>
+          <input
+            name="phone_number"
+            value={user.phone_number}
+            onChange={formChange}
+          />
+        </div>
+        <div>
+        <label htmlFor="email">
+            이메일<small>* </small>
+          </label>
+          <input name="email" value={user.email} onChange={formChange} />
+          <Button onClick={validateEmail}>이메일 인증</Button>
+        </div>
+        <div>
+        <label htmlFor="address">
+            주소<small>* </small>
+          </label>
+          <input name="address" value={user.address} onChange={formChange} />
+        </div>
+      </Form>
+      <Button onClick={submitJoin}>Join</Button>
+    </div>
+  );
+};
 export default SignUp;
