566290e842f8db033aaa9b2aceee32933237fd57
Heojiwoo <anna001030@naver.com>
2024-02-15 17:42:41 +0900

    240215 APP.js 고정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,38 +19,38 @@ import ChatPage from './chat/page/ChatPage';
 const App = () => {
   return (
 
-    <>
-    <Header className="header" />
-    <Routes>
-    <Route path="/admin" Component={Admin}></Route>
-        <Route path='/chat' Component={ChatPage}></Route>
-    </Routes>
-
-    </>
     // <>
-    //   <div className="AppBox">
-    //     <Header className="header" />
-    //     <div className="menu">
-    //       <Sidebar />
-    //     </div>
-    //     <div className="content">
-    //       <Routes>
-    //         <Route path="/" element={<Navigate to="/home"></Navigate>}></Route>
-    //         <Route path="/home" Component={Home}></Route>
-    //         <Route path="user/login" Component={Login}></Route>
-    //         <Route path="user/signup" Component={SignUp}></Route>
-    //         <Route path="user/passwordcheck" Component={PasswordCheck}></Route>
-    //         <Route path="user/update" Component={Update}></Route>
-
-    //         <Route path="/product/list" Component={ListPage}></Route>
-    //         <Route path="/product/write" Component={WritePage}></Route>
-    //         <Route path="/product/detail:id" Component={DetailPage}></Route>
-    //         <Route path="/product/update:id" Component={UpdatePage}></Route>
-    //         <Route path="/admin" Component={Admin}></Route>
-    //       </Routes>
-    //     </div>
-    //   </div>
+    // <Header className="header" />
+    // <Routes>
+    // <Route path="/admin" Component={Admin}></Route>
+    //     <Route path='/chat' Component={ChatPage}></Route>
+    // </Routes>
+    //
     // </>
+    <>
+      <div className="AppBox">
+        <Header className="header" />
+        <div className="menu">
+          <Sidebar />
+        </div>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Navigate to="/home"></Navigate>}></Route>
+            <Route path="/home" Component={Home}></Route>
+            <Route path="user/login" Component={Login}></Route>
+            <Route path="user/signup" Component={SignUp}></Route>
+            <Route path="user/passwordcheck" Component={PasswordCheck}></Route>
+            <Route path="user/update" Component={Update}></Route>
+
+            <Route path="/product/list" Component={ListPage}></Route>
+            <Route path="/product/write" Component={WritePage}></Route>
+            <Route path="/product/detail:id" Component={DetailPage}></Route>
+            <Route path="/product/update:id" Component={UpdatePage}></Route>
+            <Route path="/admin" Component={Admin}></Route>
+          </Routes>
+        </div>
+      </div>
+    </>
   );
 };
 
