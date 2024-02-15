ee682aedd13d9dba87ddd0c4979e6d1b2adf0bfe
이종현 <rmaskdkfhk@gmail.com>
2024-02-15 16:42:28 +0900

    chat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,37 +17,37 @@ import UpdatePage from "./product/page/UpdatePage"
 const App = () => {
   return (
 
-    <>
-    <Header className="header" />
-    <Routes>
-    <Route path="/admin" Component={Admin}></Route>
-    </Routes>
-  
-    </>
     // <>
-    //   <div className="AppBox">
-    //     <Header className="header" />
-    //     <div className="menu">
-    //       <Sidebar />
-    //     </div>
-    //     <div className="content">
-    //       <Routes>
-    //         <Route path="/" element={<Navigate to="/home"></Navigate>}></Route>
-    //         <Route path="/home" Component={Home}></Route>
-    //         <Route path="user/login" Component={Login}></Route>
-    //         <Route path="user/signup" Component={SignUp}></Route>
-    //         <Route path="user/passwordcheck" Component={PasswordCheck}></Route>
-    //         <Route path="user/update" Component={Update}></Route>
-
-    //         <Route path="/product/list" Component={ListPage}></Route>
-    //         <Route path="/product/write" Component={WritePage}></Route>
-    //         <Route path="/product/detail:id" Component={DetailPage}></Route>
-    //         <Route path="/product/update:id" Component={UpdatePage}></Route>
-    //         <Route path="/admin" Component={Admin}></Route>
-    //       </Routes>
-    //     </div>
-    //   </div>
+    // <Header className="header" />
+    // <Routes>
+    // <Route path="/admin" Component={Admin}></Route>
+    // </Routes>
+  
     // </>
+    <>
+      <div className="AppBox">
+        <Header className="header" />
+        <div className="menu">
+          <Sidebar />
+        </div>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Navigate to="/home"></Navigate>}></Route>
+            <Route path="/home" Component={Home}></Route>
+            <Route path="user/login" Component={Login}></Route>
+            <Route path="user/signup" Component={SignUp}></Route>
+            <Route path="user/passwordcheck" Component={PasswordCheck}></Route>
+            <Route path="user/update" Component={Update}></Route>
+
+            <Route path="/product/list" Component={ListPage}></Route>
+            <Route path="/product/write" Component={WritePage}></Route>
+            <Route path="/product/detail:id" Component={DetailPage}></Route>
+            <Route path="/product/update:id" Component={UpdatePage}></Route>
+            <Route path="/admin" Component={Admin}></Route>
+          </Routes>
+        </div>
+      </div>
+    </>
   );
 };
 
