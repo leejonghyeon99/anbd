99b8606227a3e25d9cb78630391127daf9a91cf0
이종현 <rmaskdkfhk@gmail.com>
2024-02-20 15:16:25 +0900

    이메일 인증 구혀 with Redis

--- backend/ERD/dml.sql
+++ backend/ERD/dml.sql
@@ -4,8 +4,8 @@ use db_anbd;
 show tables;
 
 desc user;
-insert into user(auth, star, phone_number, nickname, name, username, password, email, thumbnail, created_at)
-values ('ROLE_USER',3.3,'01012341234','test_name','이종현','가나다라','1234','1z122f7@email.com','default.jpeg', now());
+insert into user(auth, star, phone_number, nickname, name, username, password,region, email, thumbnail, created_at)
+values ('ROLE_USER',3.3,'01012341234','test_name','이종현','가나다라','1234','송파구','rmaskdkfhk@gmail.com','default.jpeg', now());
 desc user;
 insert into category(name) value ('의류');
 insert into category(name) value ('가전');
@@ -17,7 +17,7 @@ VALUES (1,20000,'SOLD',1, now(), '테스트 상품2', '테스트 상품입니다
 insert into product(category_id, price, status, user_id, created_at, title, description, location, middle_category)
 VALUES (2,20000,'SALE',1, now(), '테스트 상품3', '테스트 상품입니다3.', '0000, 0000', '냉장고');
 insert into product(category_id, price, status, user_id, created_at, title, description, location, middle_category)
-VALUES (2, 25500, 'SALE', 5, now(), '테스트 상품5', '테스트 상품입니다3.', '0000, 0000', '냉장고');
+VALUES (2, 25500, 'SALE', 1, now(), '테스트 상품5', '테스트 상품입니다3.', '0000, 0000', '냉장고');
 
 
 select * from information_schema.KEY_COLUMN_USAGE where TABLE_NAME = 'user_block';
