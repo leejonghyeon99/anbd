9e22eb392ac12585dfb345c718042a152ff06625
soo <104402980+kwonheesoo@users.noreply.github.com>
2024-02-23 15:43:57 +0900

    카테고리 수정 & 상세페이지

--- backend/src/main/java/com/lec/spring/service/product/ProductService.java
+++ backend/src/main/java/com/lec/spring/service/product/ProductService.java
@@ -6,8 +6,6 @@ import com.lec.spring.domain.ProductImage;
 import com.lec.spring.domain.User;
 import com.lec.spring.dto.CategoryDTO;
 import com.lec.spring.dto.ProductDTO;
-import com.lec.spring.dto.category.MainCategoryDTO;
-import com.lec.spring.dto.category.SubCategoryDTO;
 import com.lec.spring.repository.CategoryRepository;
 import com.lec.spring.repository.UserRepository;
 import com.lec.spring.repository.product.ProductImageRepository;
@@ -71,7 +69,6 @@ public class ProductService {
 //                .map(product -> ProductDTO.toDto(product))
 //                .collect(Collectors.toList());
             return null;
-
     }
 
     // 상세
@@ -92,7 +89,11 @@ public class ProductService {
     public ProductDTO update(ProductDTO product) {
         System.out.println(product.toString());
         Product productEntity = productRepository.findById(product.getId()).orElse(null);
-        Category category = categoryRepository.findById(product.getCategory().getId()).orElse(null);
+//        Category category = categoryRepository.findById(product.getCategory().getId()).orElse(null);
+
+        String main = product.getCategory().getMain();
+        String sub = product.getCategory().getSub();
+        Category category = categoryRepository.findUnique(main, sub);
 
         if (category != null) {
             category.setMain(product.getCategory().getMain());
@@ -107,9 +108,9 @@ public class ProductService {
             productEntity.setDescription(product.getDescription());
             productEntity.setCategory(category);
             productEntity.setRefreshedAt(product.getRefreshedAt());  // 끌어올리기
+
             productRepository.save(productEntity);
         }
-
         return ProductDTO.toDto(productEntity);
     }
 
@@ -122,28 +123,17 @@ public class ProductService {
         return "OK";
     }
 
-    // 특정main으로 sub 가져오기 카테고리
+    // 특정 main으로 sub 가져오기 카테고리
     public List<CategoryDTO> findByMainForSub (String main){
         System.out.println("main : ========================" + main);
-
-
         return CategoryDTO.toDtoList(categoryRepository.findAllGroupMain(main).orElse(null));
-
     }
-
     // main만 가져오기 카테고리
     public List<CategoryDTO> findByMainForList (){
-
         return CategoryDTO.toDtoList( categoryRepository.findAllOnlyMain().orElse(null));
-
-
     }
     // sub만 가져오기 카테고리
     public List<CategoryDTO> findBySubForList (){
-
         return CategoryDTO.toDtoList( categoryRepository.findAllOnlySub().orElse(null));
-
-
     }
-
 }
