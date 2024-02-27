41a26b0f5234a5947b1af9ed2aa9b28ead5b856e
GOUN <goun3596@gmail.com>
2024-02-27 10:20:23 +0900

    02.27 UpdatePassword.js 수정완료

--- backend/ERD/dml.sql
+++ backend/ERD/dml.sql
@@ -8,11 +8,12 @@ select * from information_schema.KEY_COLUMN_USAGE where TABLE_NAME = 'user_block
 UPDATE user
 SET created_at = DATE_SUB(created_at, INTERVAL 2 DAY )
 WHERE id = 17;
-
+update user set auth = 'ROLE_USER' where id = 1;
 select  * from category;
 select * from refresh_token;
 
 select * from user;
+
 
 desc user;
 
