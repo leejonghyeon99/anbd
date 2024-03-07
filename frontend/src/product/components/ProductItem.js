6a0319859d17b47dee6c80dad5d53b1b49beb475
rlagydms2 <rlagydms1@gmail.com>
2024-03-07 13:47:34 +0900

    0307 ListPage style card정렬

--- frontend/src/product/components/ProductItem.js
+++ frontend/src/product/components/ProductItem.js
@@ -1,30 +1,29 @@
 import React from "react";
 import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import "../CSS/ListPage.css"
+import "../CSS/ListPage.css";
 
 const ProductItem = (props) => {
   const { id, user, title, price } = props.product;
 
   return (
     <>
-      <Card style={{ fontFamily: "IBMPlexSansKR-Regular", padding: "1rem"}}>
-        <Card.Title 
-        style={{ fontSize: "75%", margin: "2%", textAlign: "right"}}>
-          판매자 [ {user} ]
-          </Card.Title>
+      <Card className="product-card">
+        <div className="card-title">
+          판매자 {user}
+          </div>
         <Card.Body>
           {/* 링크를 클릭하면 해당 상품의 상세 페이지로 이동 */}
           <Link to={`/product/detail/${id}`}>
             <Card.Img
               variant="top"
               src="/icon/search.png"
-              style={{ width: "90%", height: "auto",  margin: "0 auto", textAlign: "center", border: "solid 1px"}}
+              className="card-img"
             />
           </Link>
         </Card.Body>
         <ListGroup>
-          <ListGroupItem className="title">{title}</ListGroupItem>
+          <ListGroupItem className="product-title">{title}</ListGroupItem>
           <ListGroupItem className="price">{price} 원</ListGroupItem>
         </ListGroup>
       </Card>
